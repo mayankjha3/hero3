a301e52f0e8727b04fe974e97a95bf37372ddd3b
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2021-06-01 14:35:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var frist_name ="Mayank";
+console.log(frist_name);
+
+var last_name ="Jha";
+console.log(last_name);
+
+var full_name = frist_name.concat(last_name);
+console.log(full_name);
+
+ var length_of_full_name = full_name.length;
+ console.log(length_of_full_name);
+
+ var a = 10;
+ var b = 5;
+ var c = a + b;
+ console.log(c);
